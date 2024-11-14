f068032c9c47c01be8022f87cd48933491809295
manisaguana <armanisaldana@gmail.com>
2024-11-14 06:53:55 -0800

    Can't modify borrowed value

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let s1 = String::from("hello");
+    let s = String::from("hello");
 
-    let len = calculate_length(&s1);
-
-    println!("The length of '{s1}' is {len}.");
+    change(&s);
 }
-fn calculate_length(s: &String) -> usize {
-    s.len()
+
+fn change(some_string: &String) {
+    some_string.push_str(", world.")
 }
